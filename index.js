6b13cdc7e63789c55dfb9ca1528f50245ee0e788
Emmah Gicheru <gicheruemmah@gmail.com>
2025-03-20 00:38:34 +0300

    Solved and passed npm test

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+  }
+  console.log(saturdayFun());
+  console.log(saturdayFun("swim"));
+
+  const mondayWork = function(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+  };
+
+  console.log(mondayWork());
+  console.log(mondayWork("finish my codegrade"));
+
+  function wrapAdjective(symbol = "*") {
+    return function(adjective = "special") {
+        return `You are ${symbol}${adjective}${symbol}!`;
+    };
+}
+
+let result = wrapAdjective("*");
+    console.log(result("a beautiful person"));
